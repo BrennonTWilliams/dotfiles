5bc9058357e0d450aa725858d1a59cce23dc5926
Brennon Williams <brennonw@gmail.com>
2025-12-03 12:54:58 -0600

    Add zoxide abbreviations for quick directory jumping
    - Add 'j' abbreviation for 'z' command
    - Add 'ji' abbreviation for 'zi' interactive mode
    - Organize navigation file with section comment

--- zsh/abbreviations/navigation.zsh
+++ zsh/abbreviations/navigation.zsh
@@ -2,6 +2,11 @@
 # Directory Navigation Abbreviations
 # ==============================================================================
 
+# Zoxide directory jumping
+abbr -S j='z'
+abbr -S ji='zi'
+
+# Parent directory shortcuts
 abbr -S ..='cd ..'
 abbr -S ...='cd ../..'
 abbr -S ....='cd ../../..'
