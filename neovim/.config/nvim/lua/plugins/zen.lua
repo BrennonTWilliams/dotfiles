1be487deab15e8c53f2fc5ea0aa425334de28062
Brennon Williams <brennonw@gmail.com>
2026-02-16 13:48:43 -0600

    Add limelight and goyo plugins for distraction-free writing

---
+++ neovim/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "junegunn/limelight.vim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>tl", "<cmd>Limelight!!<cr>", desc = "Toggle limelight" },
+    },
+  },
+  {
+    "junegunn/goyo.vim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>tz", "<cmd>Goyo<cr>", desc = "Toggle zen mode" },
+    },
+  },
+}
