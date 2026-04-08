baa7ce8536d8f540dba06a0cd35d5108d09a6068
Brennon Williams <brennonw@gmail.com>
2026-04-08 15:24:25 -0500

    feat(zsh): add git log codebase analysis abbreviations
    Add glchurn, glwho, glbugs, glvel, and glfire abbreviations for
    quick codebase health analysis from the command line.

--- zsh/abbreviations/git.zsh
+++ zsh/abbreviations/git.zsh
@@ -7,10 +7,17 @@
 abbr -S --force --quiet gs='git status'
 abbr -S ga='git add'
 abbr -S gc='git commit'
-abbr -S gcm='git commit -m'
+abbr -S gcm='git commit -m "'
 abbr -S gp='git push'
 abbr -S gl='git log --oneline --graph --decorate'
 abbr -S gd='git diff'
 abbr -S gco='git checkout'
 abbr -S gcb='git checkout -b'
 abbr -S gb='git branch'
+
+# -- Analysis
+abbr -S glchurn="git log --after=\"1 year ago\" --name-only --pretty=format: | grep -v '^$' | sort | uniq -c | sort -nr | head -20"
+abbr -S glwho="git log --no-merges --pretty=format:%an | sort | uniq -c | sort -nr"
+abbr -S glbugs="git log -i -E --grep='fix|bug|broken' --name-only --pretty=format: | grep -v '^$' | sort | uniq -c | sort -nr | head -20"
+abbr -S glvel="git log --pretty=format:%ad --date=format:%Y-%m | sort | uniq -c"
+abbr -S glfire="git log --after=\"1 year ago\" -i -E --grep='revert|hotfix|emergency|rollback' --oneline"
